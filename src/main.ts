5557302f5bf8d710ead5ceb3a4ba146c684fa102
Sardorbek <jabborovsardor21@gmail.com>
2025-06-04 00:43:41 +0500

    feat: pinia

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import '@/assets/index.css'
+import { createPinia } from 'pinia'
 import router from '@/router'
+
+const pinia = createPinia()
 
 createApp(App)
     .use(router)
+    .use(pinia)
     .mount('#app')
